06dc6bba02fd2c5e4413b0522c45d899ea7d29cb
Albert Florido <Albertf313@gmail.com>
2023-10-20 15:49:15 -0400

    Added constants and variables, subject to change.

--- CLIStoryTemplate/main.swift
+++ CLIStoryTemplate/main.swift
@@ -5,6 +5,21 @@
 
 import Foundation
 
+let princess: String = "Cinderella"
+let stepSisters = ["Anastasia, Drizella"]
+var realSlippers: Bool = true
+var fakeSlippers: Bool = true
+var isEngaged: Bool = true
+
+
+
+
+
+
+
+
+
+
 func readStory() {
     chapterOne()
     chapterTwo()
