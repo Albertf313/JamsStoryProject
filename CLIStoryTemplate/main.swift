a89c5284fe7cf41fd634f5250ed570b82e98c137
Dashawn <dashawncmorrow@gmail.com>
2023-10-25 16:27:04 -0400

    added everything from let protagonist to "lavish"

--- CLIStoryTemplate/main.swift
+++ CLIStoryTemplate/main.swift
@@ -15,11 +15,17 @@ let fairyLocation: String = "FGM Cobbleshop"
 let prince: String = "Prince Charles"
 let FGM: String = "Fairy God Mother"
 let noMoney: Int = 0
-
-
-
-
-
+let protagonist = "Cinderella"
+let anagonist = "Wicked Family"
+let antagonist1 = "Stepmother"
+let antagonist2 = "Slender"
+let antagonist3 = "Stout"
+var isMarried = true
+var isWearingSlippers = true
+var wearingRealSlippers = true
+var wearingFakeSlippers = true
+var protagFinalStatus = "plebeian"
+var antagFinalStatus = "lavish"
 
 
 
