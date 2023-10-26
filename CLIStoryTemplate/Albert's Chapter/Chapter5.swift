c368b877417b3fe42cb8a6cf1d34d01055ca7160
Albert Florido <Albertf313@gmail.com>
2023-10-26 14:15:33 -0400

    Updated structer and functions to make it easier to read and follow.

--- CLIStoryTemplate/Albert's Chapter/Chapter5.swift
+++ CLIStoryTemplate/Albert's Chapter/Chapter5.swift
@@ -7,14 +7,11 @@ import Foundation
 
 func chapterFive() {
     // Your portion of the story goes here
-    print("the following morning, \(princess) is banished from the castle! She is stripped of all her wealth and status.")
+
    checkBankAccount()
-    print("In walks \(prince)...")
     divorce()
-    print("While walking down the road aimlessly, defeated and heartbroken, \(princess) notices \(fairyLocation). I know that old hag has something to do with this, she said.")
     checkIfShopIsOpen()
     checkIfGodMotherIsThere()
-    print("As she walks around the shop, she looks out the windows and notices \(evilSisters) walking out of the tailor shop with brand new beautiful gowns")
     checkSocialStatus()
     notSoHappilyEverAfter()
     
@@ -32,13 +29,13 @@ func chapterFive() {
 
 func checkBankAccount() {
     if princessWealthArray.contains(0) {
-        print("I'm broke! What ever will I do now? I really have \(moneyAmount) dollars and \(moneyAmount) cents.")
+        print("the following morning, \(princess) is banished from the castle! She is stripped of all her wealth and status. I'm broke! What ever will I do now? I really have \(moneyAmount) dollars and \(moneyAmount) cents.")
     }
 }
         
         func divorce() {
             if isEngaged == false {
-                print("\(princess) The wedding is off said \(prince). I cannot believe you've done this. It's over \(princess), smell you later.")
+                print("In walks \(prince)... \(princess) The wedding is off said \(prince). I cannot believe you've done this. It's over \(princess), smell you later.")
             } else {
                 print("JK You da best we're still getting married")
             }
@@ -46,7 +43,7 @@ func checkBankAccount() {
         
 func checkIfShopIsOpen() {
     if FGMShopOpen == true {
-        print("I'm gonna go in there and give her a piece of my mind." + "I know she had a hand to play in all this.")
+        print("While walking down the road aimlessly, defeated and heartbroken, \(princess) notices \(fairyLocation). I know that old hag has something to do with this, she said.I'm gonna go in there and give her a piece of my mind." + "I know she had a hand to play in all this.")
     }
 }
     
@@ -54,7 +51,7 @@ func checkIfGodMotherIsThere() {
     if fairyMotherPresent == true {
         print("You evil witch! I know you had something to do with my divorce! Why I oughta!!!")
     } else {
-        print("\(princess) Walks into the \(fairyLocation) She's no where to be seen... \(princess) walks around the shop curiously, she notices many magical treats and trinkets. She comes across many glass slippers, one pair looking oddly familiar. The sight of her past life that is now dead brings her to an even lower low.")
+        print(" \(princess) Walks into the \(fairyLocation) She's no where to be seen... \(princess) walks around the shop curiously, she notices many magical treats and trinkets. She comes across many glass slippers, one pair looking oddly familiar. The sight of her past life that is now dead brings her to an even lower low. As she walks around the shop, she looks out the windows and notices \(evilSisters) walking out of the tailor shop with brand new beautiful gowns.")
     }
 }
     
