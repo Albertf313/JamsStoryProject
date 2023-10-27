c76c741e78d1c70209398b7f11764ea83945fec9
Albert Florido <Albertf313@gmail.com>
2023-10-27 02:40:05 -0400

    Figured out the inputs. Added quotation fixes as well.

--- CLIStoryTemplate/Albert's Chapter/Chapter5.swift
+++ CLIStoryTemplate/Albert's Chapter/Chapter5.swift
@@ -13,7 +13,7 @@ func chapterFive() {
     checkIfShopIsOpen()
     checkIfGodMotherIsThere()
     checkSocialStatus()
-    notSoHappilyEverAfter()
+    notSoHappilyEverAfter(princess: "Cinderella", evilsisters: "Anastasia and Drizella", FGM: "Fairy God Mother")
     
     
     
@@ -29,13 +29,13 @@ func chapterFive() {
 
 func checkBankAccount() {
     if princessWealthArray.contains(0) {
-        print("the following morning, \(princess) is banished from the castle! She is stripped of all her wealth and status. I'm broke! What ever will I do now? I really have \(moneyAmount) dollars and \(moneyAmount) cents.")
+        print("the following morning, \(princess) is banished from the castle! She is stripped of all her wealth and status. \"I'm broke! What ever will I do now? I really have \(moneyAmount) dollars and \(moneyAmount) cents.\"")
     }
 }
         
         func divorce() {
             if isEngaged == false {
-                print("In walks \(prince)... \(princess) The wedding is off said \(prince). I cannot believe you've done this. It's over \(princess), smell you later.")
+                print("In walks \(prince)... \"\(princess) The wedding is off said \(prince). I cannot believe you've done this. It's over \(princess), smell you later.\"")
             } else {
                 print("JK You da best we're still getting married")
             }
@@ -43,7 +43,7 @@ func checkBankAccount() {
         
 func checkIfShopIsOpen() {
     if FGMShopOpen == true {
-        print("While walking down the road aimlessly, defeated and heartbroken, \(princess) notices \(fairyLocation). I know that old hag has something to do with this, she said.I'm gonna go in there and give her a piece of my mind." + "I know she had a hand to play in all this.")
+        print("While walking down the road aimlessly, defeated and heartbroken, \(princess) notices the \(fairyLocation). \"I know that old hag has something to do with this\", she said. \"I'm gonna go in there and give her a piece of my mind.\"" + "\"I know she had a hand to play in all this.\"")
     }
 }
     
@@ -57,12 +57,12 @@ func checkIfGodMotherIsThere() {
     
 func checkSocialStatus() {
     if protagFinalStatus == "plebeian" {
-        print("\(princess) falls to her knees after \(evilSisters) in their new gowns. She begs and pleads to them for help, they laugh and deny her any alms. 'However, there may be something for her', said \(evilSisters). 'In exchange for being our servant, you can live in the basement with the other pests.'.")
+        print("\(princess) falls to her knees after \(evilSisters) in their new gowns. She begs and pleads to them for help, they laugh and deny her any alms. \"However, there may be something for her\", said \(evilSisters). \"In exchange for being our servant, you can live in the basement with the other pests.\"")
     }
 }
 
-func notSoHappilyEverAfter() {
-    print("\(princess) lived the rest of her days in the basement of her evil step familys house, from rags to riches, she never learned the truth of that night. How \(evilSisters) sold her slippers to the \(FGM), where they sit behind the shops window, glistening, for all to see. ")
+func notSoHappilyEverAfter(princess: String, evilsisters: String, FGM: String) {
+    print ("\(princess) lived the rest of her days in the basement of her evil step familys house, from rags to riches, she never learned the truth of that night. How \(evilSisters) sold her slippers to the \(FGM), where they sit behind the shops window, glistening, for all to see. ")
 }
     
     
