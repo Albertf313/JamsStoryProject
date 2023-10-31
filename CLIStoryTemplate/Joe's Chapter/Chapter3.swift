b9cd97842b15d16f8534f8765221ad0dd382773e
Joe Wolf <wolf_joe@icloud.com>
2023-10-31 16:36:45 -0400

    add array

--- CLIStoryTemplate/Joe's Chapter/Chapter3.swift
+++ CLIStoryTemplate/Joe's Chapter/Chapter3.swift
@@ -7,13 +7,13 @@ import Foundation
 
 func chapterThree() {
     // Your portion of the story goes here
-    print("Chapter begins with Princess on her way to the restroom, the trio lagging not too far behind.")
+    print("Chapter begins with \(characterNames[0]) on her way to the restroom, the trio \(characterNames[1])lagging not too far behind.")
     
-    print("While the Princess is in the restroom unloosing the caboose, the three covetous women attempt to take her glass slippers from the shoe cupboard and replace them with the cheap plastic knockoffs, but fail due to hearing the latrine flush sooner than expected.")
+    print("While the \(characterNames[0]) is in the restroom unloosing the caboose, \(characterNames[1]) attempt to take her glass slippers from the shoe cupboard and replace them with the cheap plastic knockoffs, but fail due to hearing the latrine flush sooner than expected.")
     
-    print("The trio spend their night looking for an opportunity to make the swap, but can’t find another time to do so.(Attempts: Offering to polish her slippers(Failure due to the princess refusing request), Disguising as the foot massager(Failure due to all eyes being on the shoes), During the shoeless contemporary dance(Failure due to shoes being locked within cage))")
+    print("\(characterNames[1]) spend their night looking for an opportunity to make the swap, but can’t find another time to do so.(Attempts: Offering to polish her slippers(Failure due to \(characterNames[0]) refusing request), Disguising as the foot massager(Failure due to all eyes being on the shoes), During the shoeless contemporary dance(Failure due to shoes being locked within cage))")
     
-    print("The Princess enjoys the remainder of the night with her Prince. ")
+    print("\(characterNames[0]) enjoys the remainder of the night with her Prince. ")
     
     
 }
