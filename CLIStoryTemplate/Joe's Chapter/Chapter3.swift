b3d6dde56c6da66ce068d158f54f9ac5b7dd2586
Joe Wolf <wolf_joe@icloud.com>
2023-10-31 16:23:07 -0400

    Added parts of the story

--- CLIStoryTemplate/Joe's Chapter/Chapter3.swift
+++ CLIStoryTemplate/Joe's Chapter/Chapter3.swift
@@ -7,5 +7,13 @@ import Foundation
 
 func chapterThree() {
     // Your portion of the story goes here
-    print("This is joes chapter")
+    print("Chapter begins with Princess on her way to the restroom, the trio lagging not too far behind.")
+    
+    print("While the Princess is in the restroom unloosing the caboose, the three covetous women attempt to take her glass slippers from the shoe cupboard and replace them with the cheap plastic knockoffs, but fail due to hearing the latrine flush sooner than expected.")
+    
+    print("The trio spend their night looking for an opportunity to make the swap, but can’t find another time to do so.(Attempts: Offering to polish her slippers(Failure due to the princess refusing request), Disguising as the foot massager(Failure due to all eyes being on the shoes), During the shoeless contemporary dance(Failure due to shoes being locked within cage))")
+    
+    print("The Princess enjoys the remainder of the night with her Prince. ")
+    
+    
 }
